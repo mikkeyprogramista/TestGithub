2efad9dc4b9c5a9a37b866a2014dfb1a452b3f89
xatrixx123 <mikkeyprogramista@gmail.com>
2025-01-27 11:13:29 +0100

    main

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,33 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Podaj pierwszą liczbę dwucyfrową: ");
+        String first = sc.nextLine();
+
+        System.out.println("Podaj drugą liczbę dwucyfrową: ");
+        String second = sc.nextLine();
+
+        boolean valid = false;
+
+        for (int i = 0; i < first.length(); i++) {
+            if (valid) {
+                break;
+            }
+
+            for (int j = 0; j < second.length(); j++) {
+                if (valid) {
+                    break;
+                }
+
+                if (first.charAt(i) == second.charAt(j)) {
+                    valid = true;
+                }
+            }
+        }
+
+        System.out.println(valid);
     }
 }
